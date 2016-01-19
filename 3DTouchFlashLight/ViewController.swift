dea14e64e35683bd50287db3e0a68585f14fdfbc
Harel Avikasis <harelavikasis@gmail.com>
2016-01-19 19:37:26 +0200

    Initial Commit

---
+++ 3DTouchFlashLight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3DTouchFlashLight
+//
+//  Created by Harel Avikasis on 19/01/2016.
+//  Copyright © 2016 Harel Avikasis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
