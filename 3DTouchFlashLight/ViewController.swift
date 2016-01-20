581c5a50586797452d5f973a55d8c5e11dcc70a8
deadsniper <harelavikasis@gmail.com>
2016-01-20 18:39:52 +0200

    First Commit
    Torch strength Controlled by 3D Touch

--- 3DTouchFlashLight/ViewController.swift
+++ 3DTouchFlashLight/ViewController.swift
@@ -7,19 +7,115 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+//    let captureSession = AVCaptureSession()
+//    var captureDevice : AVCaptureDevice?
+    let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+    
+    @IBOutlet weak var Slider: UISlider!
+    
+    @IBOutlet weak var valueLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first where traitCollection.forceTouchCapability == .Available {
+            if (device.hasTorch) {
+                let cgFloat1: CGFloat = touch.force
+                let cgFloat2: CGFloat = touch.maximumPossibleForce
+                let currentValue = Float(cgFloat1 / cgFloat2)
+                do {
+                    try device.lockForConfiguration()
+                    if (device.torchMode == AVCaptureTorchMode.On) {
+                        //                    device.torchMode = AVCaptureTorchMode.Off
+                        if(currentValue <= 0.001) {
+                            valueLabel.text = "\(0)"
+                            device.torchMode = AVCaptureTorchMode.Off
+                        } else if(currentValue >= 1.0){
+                            try device.setTorchModeOnWithLevel(1.0)
+                            valueLabel.text = "\(1.0)"
+                        }else{
+                            try device.setTorchModeOnWithLevel(currentValue)
+                            valueLabel.text = "\(currentValue)"
+                        }
+                    } else {
+                        if(currentValue <= 0.001) {
+                            valueLabel.text = "\(0)"
+                            device.torchMode = AVCaptureTorchMode.Off
+                        } else if(currentValue >= 1.0){
+                            try device.setTorchModeOnWithLevel(1.0)
+                            valueLabel.text = "\(1.0)"
+                        }else{
+                            try device.setTorchModeOnWithLevel(currentValue)
+                            valueLabel.text = "\(currentValue)"
+                        }
 
+                    }
+                    device.unlockForConfiguration()
+                } catch {
+                    print(error)
+                }
+            }
+
+        }
+    }
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if (device.hasTorch) {
+            do {
+                try device.lockForConfiguration()
+                if (device.torchMode == AVCaptureTorchMode.On) {
+                    device.torchMode = AVCaptureTorchMode.Off
+                    valueLabel.text = "\(0)"
+                }
+                device.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+   
+    @IBAction func valueChanged(sender: AnyObject) {
+        let currentValue = Slider.value
+        if (device.hasTorch) {
+            do {
+                try device.lockForConfiguration()
+                if (device.torchMode == AVCaptureTorchMode.On) {
+//                    device.torchMode = AVCaptureTorchMode.Off
+                    if(currentValue == 0.001) {
+                        valueLabel.text = "\(0)"
+                        device.torchMode = AVCaptureTorchMode.Off
+                    } else {
+                        try device.setTorchModeOnWithLevel(currentValue)
+                        valueLabel.text = "\(currentValue)"
+                    }
+                } else {
+                    if(currentValue == 0.001) {
+                        valueLabel.text = "\(0)"
+                       device.torchMode = AVCaptureTorchMode.Off
+                    } else {
+                        try device.setTorchModeOnWithLevel(currentValue)
+                        valueLabel.text = "\(currentValue)"
+                    }
+                }
+                device.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        }
+        
+    }
 
 }
 
